f3d99cff55afa52e0d2339654f439d2705e5ab6c
Greg Detre <greg@gregdetre.co.uk>
2014-01-24 10:55:43 -0500

    Tweak

--- rbmgreg/utils.py
+++ rbmgreg/utils.py
@@ -21,7 +21,7 @@ def isunique(lst): return len(set(lst))==1
 
 def sigmoid(x):
     # from peter's rbm
-    return 1.0 / (1.0 + np.exp(-1.0 * x))
+    return 1.0 / (1.0 + np.exp(-x))
 
 def sumsq(x): return sum(x**2)
 
