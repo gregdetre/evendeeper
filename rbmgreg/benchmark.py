ff3e2e823e871af3cdaad4bd83d4c1fb576d9d18
Greg Detre <greg@gregdetre.co.uk>
2014-01-24 20:23:41 -0500

    Added command-line arguments to benchmark.py

--- rbmgreg/benchmark.py
+++ rbmgreg/benchmark.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from ipdb import set_trace as pause
 import numpy as np
 import os
@@ -7,6 +8,19 @@ from rbm import create_mnist_patternset, RbmNetwork
 from utils.dt import dt_str
 from utils.stopwatch import Stopwatch
 from utils.utils import HashableDict
+
+"""
+USAGE: e.g.
+
+  python benchmark.py -h
+
+  python benchmark.py -s 3 --lrate 0.001 --lrate 0.01 --momentum 0.1 --momentum 0.2 --nhidden 1000
+  ->
+    {'nhidden': 1000, 'wcost': 0.0002, 'momentum': 0.1, 'lrate': 0.001}
+    {'nhidden': 1000, 'wcost': 0.0002, 'momentum': 0.2, 'lrate': 0.001}
+    {'nhidden': 1000, 'wcost': 0.0002, 'momentum': 0.1, 'lrate': 0.01}
+    {'nhidden': 1000, 'wcost': 0.0002, 'momentum': 0.2, 'lrate': 0.01}
+"""
 
 # TODO
 #
@@ -21,37 +35,37 @@ def print_best(attempts, keys):
     for rank, attempt in enumerate(attempts):
         print ('%i\t%.2f\t' % (rank+1, attempt['test_error'])) +'\t'.join([str(attempt[k]) for k in keys])
 
-
 def sorted_attempts(attempts):
     attempts = sorted(attempts, key=lambda x: x['test_error'])
     return attempts
-        
 
-if __name__ == "__main__":
+def gridsearch(nhidden, lrate, wcost, momentum, n_in_train_minibatch, max_time_secs):
     npatterns = 1000
     pset = create_mnist_patternset(npatterns=npatterns)
     # xxx - this isn't a proper test since we're using the training data, rather than withheld...
     n_in_test_minibatch = 1000
     test_minibatch = Minibatch(pset, n_in_test_minibatch)
-    max_time_secs = 100 # how long to give each ATTEMPT
 
     params = ['nhidden', 'lrate', 'wcost', 'momentum', 'n_in_train_minibatch']
     attempts = []
-    for nhidden in [100]: # [200, 400, 800]:
-        for lrate in [0.005]: # [0.005, 0.02]:
-            for wcost in [0, 0.0002]: #, 0.002]:
-                for momentum in [0, 0.4, 0.9]:
+    for nh in nhidden: # [200, 400, 800]:
+        for lr in lrate: # [0.005, 0.02]:
+            for wc in wcost: #, 0.002]:
+                for mo in momentum: # [0, 0.4, 0.9]:
                     for n_in_train_minibatch in [20]: # [10, 250]:
-                        attempts.append({'nhidden': nhidden,
-                                         'lrate': lrate,
-                                         'wcost': wcost,
-                                         'momentum': momentum,
+                        attempts.append({'nhidden': nh,
+                                         'lrate': lr,
+                                         'wcost': wc,
+                                         'momentum': mo,
                                          'should_plot': False,
                                          })
     
     nattempts = len(attempts)
     pid = os.getpid()
+    for attempt in attempts: print attempt
+    import sys; sys.exit(0)
     print 'Beginning %i attempts (PID=%s, DT=%s), each for %i secs' % (nattempts, pid, dt_str(), max_time_secs)
+
     all_t = Stopwatch()
     for attempt_idx, attempt in enumerate(attempts):
         np.random.seed(1)
@@ -95,3 +109,23 @@ if __name__ == "__main__":
     if all_t.finish(milli=False) > 1000: pause()
 
     
+if __name__ == "__main__":
+    parser = ArgumentParser(description=__doc__)
+    parser.add_argument('--nhidden', type=int, default=[], action='append', help='Number of hidden units')
+    parser.add_argument('--lrate', type=float, default=[], action='append', help='Learning rate')
+    parser.add_argument('--wcost', type=float, default=[], action='append', help='Weight cost')
+    parser.add_argument('--momentum', type=float, default=[], action='append', help='Momentum')
+    parser.add_argument('--n_in_train_minibatch', type=int, default=[], action='append', help='Minibatch size (during training)')
+    parser.add_argument('-s', '--max_time_secs', type=int, default=100, help='How much time to run each attempt for')
+#     parser.add_argument('--shouldplot', type=int, default=0, help='How often to plot graphs - default is 0, i.e. never')
+    kwargs = vars(parser.parse_args())
+    # i haven't figured out a better way to include defaults
+    # in ArgParse (without them still getting included even
+    # when you try and override them)
+    defaults = {'nhidden': 100,
+                'lrate': 0.005,
+                'wcost': 0.0002,
+                'momentum': 0.9,}
+    for k,v in defaults.items():
+        if kwargs.get(k) == []: kwargs[k].append(v)
+    gridsearch(**kwargs)
