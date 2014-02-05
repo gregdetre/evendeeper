42e24936d9e5b5ec5ae0ed7d8257379c9e2c0aa0
Greg Detre <greg@gregdetre.co.uk>
2014-02-05 16:19:23 -0500

    Tweaks to backprop

--- rbm/backprop.py
+++ rbm/backprop.py
@@ -3,7 +3,7 @@ import numpy as np
 from random import shuffle
 
 from base import create_mnist_patternset, Minibatch2, Network, Patternset
-from utils.utils import deriv_sigmoid, imagesc, sigmoid, sumsq, vec_to_arr
+from utils.utils import deriv_sigmoid, deriv_tanh, imagesc, sigmoid, sumsq, tanh, vec_to_arr
 
 
 class BackpropNetwork(Network):
@@ -16,7 +16,7 @@ class BackpropNetwork(Network):
         self.momentum = momentum
         self.lrate = lrate
 
-    def init_weights(self, scale=0.1):
+    def init_weights(self, scale=0.01):
         return [np.random.normal(size=(n1,n2), scale=scale) if scale else np.zeros((n1,n2))
                 for n1,n2 in zip(self.layersizes, self.layersizes[1:])]
 
@@ -38,7 +38,7 @@ class BackpropNetwork(Network):
         w, b = self.w[lowlayer_idx], self.b[lowlayer_idx+1]
         return super(BackpropNetwork, self).propagate_fwd(act, w, b)
 
-    def report_error(self, act_k, tgt): return np.sum((tgt - act_k)**2, axis=1)
+    def report_error(self, act_k, tgt): return np.sum(np.power(tgt - act_k, 2), axis=1)
 
     def learn_trial(self, act0, target):
         d_w, d_b = self.delta_weights(act0, target)
@@ -79,8 +79,11 @@ class BackpropNetwork(Network):
 
     def act_fn(self, x): return sigmoid(x)
     def deriv_act_fn(self, x): return deriv_sigmoid(x)
+
+#     def act_fn(self, x): return tanh(x)
+#     def deriv_act_fn(self, x): return deriv_tanh(x)
     
-    def err_fn(self, actual, desired): return (actual - desired)**2
+    def err_fn(self, actual, desired): return np.pow(actual - desired, 2)
 
 
 def arr_str(arr): return np.array_str(arr, precision=2)
@@ -120,9 +123,9 @@ def rand_autoencoder(nhiddens=None, *args, **kwargs):
     pset = Patternset([np.random.uniform(size=(1,n_inp_out)) for d in range(npatterns)])
     return autoencoder(pset, nhiddens=nhiddens, *args, **kwargs)
 
-def mnist_autoencoder(npatterns=None, nhiddens=None, *args, **kwargs):
+def mnist_autoencoder(npatterns=None, nhiddens=None, zero_to_negone=False, *args, **kwargs):
     if nhiddens is None: nhiddens = [500]
-    pset = create_mnist_patternset(npatterns=npatterns, ravel=True)
+    pset = create_mnist_patternset(npatterns=npatterns, ravel=True, zero_to_negone=zero_to_negone)
     return autoencoder(pset, nhiddens=nhiddens, *args, **kwargs)
 
 
@@ -130,10 +133,11 @@ if __name__ == "__main__":
     np.random.seed()
     # net, iset, oset = xor(lrate=0.35)
     # net, iset, oset = rand_autoencoder(nhiddens=[24, 12], lrate=0.1)
-    net, iset, oset = mnist_autoencoder(npatterns=None, nhiddens=[500], lrate=0.05)
-    nEpochs = 2000 # 100000
+    # net, iset, oset = mnist_autoencoder(npatterns=1000, nhiddens=[500, 100], lrate=0.01)
+    net, iset, oset = mnist_autoencoder(npatterns=None, nhiddens=[200], zero_to_negone=True, lrate=0.01)
+    nEpochs = 20000 # 100000
     n_in_minibatch = min(len(iset), 20)
-    report_every = 1 # 1000
+    report_every = 100 # 1000
     for e in range(nEpochs):
         act0, target = Minibatch2(iset, oset, n_in_minibatch).patterns
         # act0, target = act0.ravel(), target.ravel()
