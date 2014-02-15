78e4cd70c79899cceef58b4c4d6c8e173aa60ee7
David Haber <david.haber90@gmail.com>
2014-02-15 13:57:31 +0000

    Initial commit for DBN

---
+++ rbm/dbn.py
@@ -0,0 +1,13 @@
+# TODO
+#
+# DBN with L layers
+#
+# Greedy layer-wise training
+#   learn the weights for each layer l
+#   pass the hidden activations as inputs to the next level
+# Up-down
+#   propagate the input all the way forward to level L
+#   k-steps of CD in the top two layers
+#   propagate back and perform positive/negative learning for each layer
+#   (calculate gradient and update weights)
+
