1713c2b689778187392d981f61d3aeb1350e6bac
David Haber <david.haber90@gmail.com>
2014-02-15 14:47:35 +0000

    Initial sketch for algorithm - not supposed to compile at this stage

--- rbm/dbn.py
+++ rbm/dbn.py
@@ -11,3 +11,50 @@
 #   propagate back and perform positive/negative learning for each layer
 #   (calculate gradient and update weights)
 
+class DBN(Network):
+
+    def __init__(self, layersizes):
+        self.up_epochs = 100000
+        self.down_epochs = 100000
+
+        self.rbms = []
+        for l1, l2 in zip(self.layersizes[:-1], self.layersizes[1:]):
+            n_v = l1
+            n_h = l2
+            rbm = RbmNetwork(n_v, n_h)
+            self.rbms.append(rbm)
+
+    def test_trial(self):
+        # go all the way up
+        # go all the way down
+        # calculate reconstruction error
+        pass
+
+    def learn_trial(self):
+        greedy_layerwise_training()
+        up_down_training()
+
+    def greedy_layerwise_training(self, wholeds):
+        layer_input = wholeds
+        for rbm in self.rbms:
+            rbm.train(layer_input)
+            layer_input = rbm.propagatefwd(layer_input)
+            
+    def up_down_training(self, wholeds):
+        up_activations = propagate_fwd_all(wholeds) #propagate all the forward to get v+ for top rbm
+        v_minus_top = run_cdk_on_top_layers(v_plus)
+        for rbm in self.rbms[-2::-1]: # iterate backwards ignoring top RBM
+            # take up_activations for this rbm
+            # take sample from higher-level rbm
+            # calculate gradient and adjust weights
+
+    def run_cdk_on_top_layers(self, v_plus):
+        # get v_plus 
+        top_rbm = self.rbms[-1]
+        v_minus = top_rbm.learn_trial(v_plus) # v_minus after k-step CD
+        return v_minus
+            
+        
+
+    
+    
